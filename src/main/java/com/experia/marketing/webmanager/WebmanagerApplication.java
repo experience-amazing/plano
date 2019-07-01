3295a417c35f5b21e31cda3216dbdebd08f0646a
gonzalb11 <benedicto.gonzales@toyota.com>
2019-07-01 15:32:27 -0500

    init

---
+++ src/main/java/com/experia/marketing/webmanager/WebmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.experia.marketing.webmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmanagerApplication.class, args);
+	}
+
+}
