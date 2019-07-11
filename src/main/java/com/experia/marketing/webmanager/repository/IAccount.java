33f9d3c4707e03075c47ddaf64fe113cf1385fca
gonzalb11 <benedicto.gonzales@toyota.com>
2019-07-11 16:21:59 -0500

    Service and Repo

--- src/main/java/com/experia/marketing/webmanager/repository/IAccount.java
+++ src/main/java/com/experia/marketing/webmanager/repository/IAccount.java
@@ -3,9 +3,11 @@ package com.experia.marketing.webmanager.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.experia.marketing.webmanager.dao.Account;
 
+@Repository
 public interface IAccount extends CrudRepository<Account, Long>  {
 
 	List<Account> findByEmail(String email);
--- src/main/java/com/experia/marketing/webmanager/repository/IProfile.java
+++ src/main/java/com/experia/marketing/webmanager/repository/IProfile.java
@@ -3,9 +3,11 @@ package com.experia.marketing.webmanager.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.experia.marketing.webmanager.dao.Profile;
 
+@Repository
 public interface IProfile extends CrudRepository<Profile, Long> {
 	List<Profile> findByAccountId(String accountId);
 }
